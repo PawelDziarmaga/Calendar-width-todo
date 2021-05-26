bbc31fc9003823fca6b63db7f3a62cf03d7739ae
Pawel D <dziarmaga8@gmail.com>
2021-05-26 20:10:29 +0200

    Create React App, create sceleton of application.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React from 'react'
+import Form from './Leyouts/Form/Form'
+import Nav from './Leyouts/Nav/Nav'
+import Body from './Leyouts/Body/Body'
+import Footer from './Leyouts/Footer/Footer'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Calendar width todo list</h1>
+      <Form />
+      <Nav />
+      <Body />
+      <Footer />
     </div>
   );
 }

fb5917435e6de7b004751478fe44da1bcfe8ad2d
Pawel D <dziarmaga8@gmail.com>
2021-06-06 14:17:03 +0200

    Add Form to add_quest

--- src/Leyouts/Form/Form.js
+++ src/Leyouts/Form/Form.js
@@ -1,9 +1,93 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './Form.css'
+let id = 0;
 
 function Form() {
+  const [quest, setQuest] = useState('')
+  const [timeStart, setTimeStart] = useState('')
+  const [timeDone, setTimeDone] = useState('')
+  const [date, setDate] = useState('')
+
+  const onChangeHandler = (e) => {
+    switch(e.target.id){
+      case 'quest':
+        return setQuest(e.target.value);
+      case 'timeStart':
+        return setTimeStart(e.target.value);
+      case 'timeDone':
+        return setTimeDone(e.target.value);
+      case 'date':
+        return setDate(e.target.value);
+      default:
+        return
+    }
+    
+  }
+  const buttonHandler = (e) => {
+    e.preventDefault()
+
+    if(!quest.length){
+      return;
+    }
+
+
+    const object ={
+      id,
+      quest,
+      date,
+      timeStart,
+      timeDone,
+    }
+    console.log(object)
+    id = id + 1;
+  }
     return (
       <div className="form">
-          Form
+          <form>
+            <div>
+              <label>
+                Nazwa zadania:
+                <input 
+                  id="quest"
+                  onChange={onChangeHandler}
+                  type="text"
+                  value={quest}
+                  />
+              </label>
+            </div>
+            <div>
+              <label>
+                Od godziny:
+                <input 
+                  id="timeStart"
+                  onChange={onChangeHandler}
+                  type="time"
+                  value={timeStart}
+                  />
+              </label>
+              <label>
+                Do godziny:
+                <input 
+                  id="timeDone"
+                  onChange={onChangeHandler}
+                  type="time"
+                  value={timeDone}
+                  />
+              </label>
+            </div>
+            <div>
+              <label>
+                Data:
+                <input 
+                  id="date"
+                  onChange={onChangeHandler}
+                  type="date"
+                  value={date}
+                  />
+              </label>
+            </div>
+            <button onClick={buttonHandler}>Dodaj zadanie</button>
+          </form>
       </div>
     );
   }

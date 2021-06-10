05f5165f42f21defeb3ec136697c54d2256ae673
Pawel D <dziarmaga8@gmail.com>
2021-06-10 21:01:14 +0200

    Add structuce of calendar

--- src/Leyouts/Calendar/Calendar.js
+++ src/Leyouts/Calendar/Calendar.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, {useState} from 'react';
+import './Calendar.css'
+
+import CalendarHeder from './CalendarHeader/CalendarHeader'
+import CalendarTable from './CalendarTable/CalendarTable'
+
 
 function Calendar() {
-    return (
+  const now = new Date();
+  const [month, setMonth] = useState(now.getMonth())
+  const [year, setYear] = useState(now.getFullYear())
+  if(month === 12){
+    setMonth(0)
+  }
+  if(month === -1){
+    setMonth(11)
+  }
+      const day = now.getDate();
+      
+      console.log("now: " + now)
+      console.log("day: " + day)
+ 
+  const handleClick = ({target}) =>{
+      if(target.className === 'nextMonth'){
+          if(month === 11){
+            setYear(prev => ++prev)
+          }
+          setMonth(prev => ++prev)
+          
+      }else if(target.className === 'prevMonth'){
+        if(month === 0){
+          setYear(prev => --prev)
+        }
+          setMonth(prev => --prev)
+        }
+
+
+    }
+
+  return (
       <div className="Calendar">
-          <h2>Kalendarz</h2>
+        <CalendarHeder click={handleClick} month={month} year={year}/>
+        <CalendarTable month={month} year={year}/>
       </div>
     );
   }
---
+++ src/Leyouts/Calendar/CalendarHeader/CalendarHeader.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './CalendarHeader.css'
+
+function CalendarHeader({click, month, year}) {
+  const months = ['stczeń', 'luty', 'marzec', 'kwiecień', 'maj', 'czerwiec', 'lipiec', 'siepień', 'wrzesień', 'pażdziernik', 'listopad', 'grudzień']
+
+  
+  return (
+      <div className="CalendarHeader">
+        <button onClick={click} className="prevMonth">prev</button>
+        <h3>{months[month]} {year}</h3>
+        <button onClick={click} className="nextMonth">next</button>
+      </div>
+    );
+  }
+  
+  export default CalendarHeader;
